dbcb8f5d294c43d5465b51a7598ab7291961f2bc
Luis Ramirez <lou81804@gmail.com>
2022-06-18 17:03:05 -0500

    Implementing CsvFileReader

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log('Hello world')
+import { CsvFileReader } from "./CsvFileReader"
+
+const reader = new CsvFileReader('football.csv')
+reader.read()
+const data = reader.data
+
+let manUnitedWins = data.filter((element) => (element[1] === 'Man United' && element[5] === 'H') || (element[2] === 'Man United' && element[5] === 'A')).length
+
+
+console.log(manUnitedWins)
